90840b30928a9b8a459cfbf3f90f4bd6c270e2e2
kkwong44 <wong.k@sky.com>
2022-05-13 18:53:03 +0000

    Build the admin site

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['codestar2022k.herokuapp.com', 'local']
+ALLOWED_HOSTS = ['codestar2022k.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'django_summernote',
     'blog',
 ]
 
